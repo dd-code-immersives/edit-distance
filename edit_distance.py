562fc0c3d6af8c6154c2b26b56e7467376d5374c
dd-code-immersives <domilamad89@gmail.com>
2022-02-02 18:24:00 +0100

    made file conform to pep8 style

--- edit_distance.py
+++ edit_distance.py
@@ -1,62 +1,62 @@
 def edit_distance(str1, str2):
+    """
+    case 1: the same length 
+    case 2: they are not the same length 
 
-	
+    """
 
-	"""
-	case 1: the same length 
-	case 2: they are not the same length 
+    # if the strings are the same, edit distance = 0
+    if str1 == str2:
+        return 0
 
-	"""
+    # the case if the strings are the same length
+    # ben, hen
+    if len(str1) == len(str2):
+        counter = 0
+        for idx, ch_ in enumerate(str1):
+            if ch_ != str2[idx]:
+                counter += 1
 
-	# if the strings are the same, edit distance = 0
-	if str1 == str2:
-		return 0 
+        return counter
 
-	# the case if the strings are the same length 
-	# ben, hen 
-	if len(str1) == len(str2):
-		counter = 0
-		for idx, ch in enumerate(str1):
-			if ch != str2[idx]:
-				counter += 1
+    if len(str1) != len(str2):
+        counter = 0
+        # add the difference
+        # i.e. the then -> 4 - 3 =  1
 
-		return counter
+        larger = None
+        smaller = None
 
-	if len(str1) != len(str2):
-		counter = 0 
-		# add the difference 
-		# i.e. the then -> 4 - 3 =  1
+        if len(str1) > len(str2):
+            larger = str1
+            smaller = str2
+        else:
+            larger = str2
+            smaller = str1
 
-		larger = None
-		smaller = None
+        offset = len(larger) - len(smaller)
+        counter += offset
 
-		if len(str1) > len(str2):
-			larger = str1
-			smaller = str2
-		else:
-			larger = str2 
-			smaller = str1
+        for idx, ch_ in enumerate(smaller):
+            if ch_ != larger[idx]:
+                counter += 1
 
-
-		offset = len(larger) - len(smaller)
-		counter += offset
-
-
-		for idx, ch in enumerate(smaller):
-			if ch != larger[idx]:
-				counter += 1
-
-		return counter
+        return counter
 
 
 if __name__ == '__main__':
-	wd1 = "the"
-	wd2 = "hen"
-	wd3 = "ben" 
-	wd4 = "then"
-	wd5 = "again"
-	print(f"The distance of '{wd1}' and '{wd1}' is { edit_distance(wd1, wd1)}")
-	print(f"The distance of '{wd2}' and '{wd3}' is { edit_distance(wd2, wd3)}")
-	print(f"The distance of '{wd1}' and '{wd4}' is { edit_distance(wd1, wd4)}")
-	print(f"The distance of '{wd1}' and '{wd4}' is { edit_distance(wd4, wd1)}")
-	print(f"The distance of '{wd1}' and '{wd5}' is { edit_distance(wd1, wd5)}")
+    WD_1 = "the"
+    WD_2 = "hen"
+    WD_3 = "ben"
+    WD_4 = "then"
+    WD_5 = "again"
+    print(
+        f"The distance of '{WD_1}' and '{WD_1}' is { edit_distance(WD_1, WD_1)}")
+    print(
+        f"The distance of '{WD_2}' and '{WD_3}' is { edit_distance(WD_2, WD_3)}")
+    print(
+        f"The distance of '{WD_1}' and '{WD_4}' is { edit_distance(WD_1, WD_4)}")
+    print(
+        f"The distance of '{WD_1}' and '{WD_4}' is { edit_distance(WD_4, WD_1)}")
+    print(
+        f"The distance of '{WD_1}' and '{WD_5}' is { edit_distance(WD_1, WD_5)}")
